c7b3737f97d68823830e0859657f42749fc84c60
Jason Abbott <dev@trailimage.com>
2017-02-21 23:08:09 -0700

    geo functions

---
+++ gps/gpx.go
@@ -0,0 +1,16 @@
+package gps
+
+type GPX struct {
+}
+
+func (gpx *GPX) Value(node string) string {
+	return ""
+}
+
+func (gpx *GPX) FirstValue(node, tag string) string {
+	return ""
+}
+
+func (gpx *GPX) FirstNode(node, tag string) string {
+	return ""
+}
